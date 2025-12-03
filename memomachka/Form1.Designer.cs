2e59a55358d8688de82bfc1c7a7e7d3aaca51be0
veljaaas <veljaaas@DESKTOP-0CADGD7>
2025-12-03 16:56:00 +0100

    Алфа верзија - 6 карата - 7 могућих слика - Обавештење о завршетку игре

---
+++ memomachka/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace memomachka
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(207, 194, 201);
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(50, 50);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 100);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(207, 194, 201);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(200, 50);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 100);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(207, 194, 201);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(350, 50);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 100);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(207, 194, 201);
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(350, 200);
+            button4.Name = "button4";
+            button4.Size = new Size(100, 100);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(207, 194, 201);
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Location = new Point(200, 200);
+            button5.Name = "button5";
+            button5.Size = new Size(100, 100);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(207, 194, 201);
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Location = new Point(50, 200);
+            button6.Name = "button6";
+            button6.Size = new Size(100, 100);
+            button6.TabIndex = 5;
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(25, 1, 36);
+            ClientSize = new Size(507, 353);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MdiChildrenMinimizedAnchorBottom = false;
+            Name = "Form1";
+            Text = "memomachka ";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
---
+++ memomachka/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace memomachka
+{
+    public partial class Form1 : Form
+    {
+
+        Image[] slika;
+        Button[] dugmad;
+        int[] slikaDugme;
+
+        private bool potezUToku;
+        int trenutni;
+        int prethodni;
+        int parovi;
+
+        Random brojGen;
+        public Form1()
+        {
+            InitializeComponent();
+            potezUToku = false;
+            parovi = 0;
+            prethodni = 1;
+
+            slika = new Image[7];
+            dugmad = new Button[6];
+            slikaDugme = new int[6];
+            brojGen = new Random();
+
+            int pom1, pom2, pom3;
+            bool[] iskoristene = { false, false, false, false, false, false };
+            bool[] iskoristeneSlike = { false, false, false, false, false, false, false };
+
+            dugmad[0] = button1;
+            dugmad[1] = button2;
+            dugmad[2] = button3;
+            dugmad[3] = button4;
+            dugmad[4] = button5;
+            dugmad[5] = button6;
+
+            slika[0] = Properties.Resources.m1;
+            slika[1] = Properties.Resources.m2;
+            slika[2] = Properties.Resources.m3;
+            slika[3] = Properties.Resources.m4;
+            slika[4] = Properties.Resources.m5;
+            slika[5] = Properties.Resources.m6;
+            slika[6] = Properties.Resources.m7;
+
+            for (int i = 0; i < 3; i++)
+            {
+                do {
+                    pom1 = brojGen.Next(1, 7);
+                } while (iskoristene[pom1 - 1] == true);
+
+                iskoristene[pom1 - 1] = true;
+
+                do
+                {
+                    pom2 = brojGen.Next(1, 7);
+                } while (iskoristene[pom2 - 1] == true);
+
+                iskoristene[pom2 - 1] = true;
+
+                do
+                {
+                    pom3 = brojGen.Next(1, 8);
+                } while (iskoristeneSlike[pom3 - 1] == true);
+
+                iskoristeneSlike[pom3 - 1] = true;
+
+                slikaDugme[pom1 - 1] = pom3 - 1;
+                slikaDugme[pom2 - 1] = pom3 - 1;
+            }
+
+
+
+        }
+
+        async void klik()
+        {
+            dugmad[trenutni-1].BackgroundImage = slika[slikaDugme[trenutni-1]];
+            if (!potezUToku)
+            {
+                potezUToku = true;
+                prethodni = trenutni;
+            }
+            else
+            { 
+                if (slikaDugme[trenutni-1] == slikaDugme[prethodni-1]) { if (++parovi == 3) MessageBox.Show("Игра је завршена :)"); }
+                else
+                {
+                    for (int i = 0; i < 6; i++) dugmad[i].Enabled = false;
+                    await Task.Delay(1000);
+                    dugmad[trenutni-1].BackgroundImage = null;
+                    dugmad[prethodni-1].BackgroundImage = null;
+                    for (int i = 0; i < 6; i++) dugmad[i].Enabled = true;
+                }
+
+                potezUToku = false;
+            }
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            trenutni = 3;
+            klik();
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            trenutni = 4;
+            klik();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            trenutni = 1;
+            klik();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            trenutni = 2;
+            klik();
+        }
+
+        private async void button5_Click(object sender, EventArgs e)
+        {
+            trenutni = 5;
+            klik();
+        }
+
+        private async void button6_Click(object sender, EventArgs e)
+        {
+            trenutni = 6;
+            klik();
+        }
+
+        
+    }
+
+
+}
